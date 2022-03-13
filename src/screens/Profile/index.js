8d84885a03379452a22955449b0ecd4b8d0f6b51
Amal001001 <amalalmutairi001@gmail.com>
2022-03-14 02:42:21 +0300

    profile UI 2

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { View, SafeAreaView, StyleSheet } from "react-native";
 import theme from "../../../theme";
 import Header from "../../components/Header";
-import { Avatar, Title, Caption, TouchableRipple } from "react-native-paper";
+import { Title, Caption, Text, TouchableRipple } from "react-native-paper";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 // import Share from 'react-native-share';
 export default class Profile extends React.Component {
@@ -12,107 +12,59 @@ export default class Profile extends React.Component {
   }
   render() {
     return (
-      <View style={styles._container}>
+       <View style={styles.container}>
         <Header title="Profile" />
-        <View style={styles.userInfoSection}>
-          <View style={{ flexDirection: "row", marginTop: 15 }}>
-            <Avatar.Image
-              source={{
-                uri: "https://api.adorable.io/avatars/80/abott@adorable.png",
-              }}
-              size={80}
-            />
-            <View style={{ marginLeft: 20 }}>
-              <Title
-                style={[
-                  styles.title,
-                  {
-                    marginTop: 15,
-                    marginBottom: 5,
-                  },
-                ]}
-              >
-                John Doe
-              </Title>
-              <Caption style={styles.caption}>@j_doe</Caption>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.userInfoSection}>
-          <View style={styles.row}>
-            <Icon name="map-marker-radius" color="#777777" size={20} />
-            <Text style={{ color: "#777777", marginLeft: 20 }}>
-              Kolkata, India
-            </Text>
-          </View>
-          <View style={styles.row}>
-            <Icon name="phone" color="#777777" size={20} />
-            <Text style={{ color: "#777777", marginLeft: 20 }}>
-              +91-900000009
-            </Text>
-          </View>
-          <View style={styles.row}>
-            <Icon name="email" color="#777777" size={20} />
-            <Text style={{ color: "#777777", marginLeft: 20 }}>
-              john_doe@email.com
-            </Text>
-          </View>
-        </View>
-
-        <View style={styles.infoBoxWrapper}>
-          <View
-            style={[
-              styles.infoBox,
-              {
-                borderRightColor: "#dddddd",
-                borderRightWidth: 1,
-              },
-            ]}
-          >
-            <Title>₹140.50</Title>
-            <Caption>Wallet</Caption>
-          </View>
+        <View style={{marginTop:5,marginBottom: 10,}}></View>
+          
+      <View style={styles.infoBoxWrapper}>
           <View style={styles.infoBox}>
-            <Title>12</Title>
-            <Caption>Orders</Caption>
+            <Title style={styles._header_title}>10</Title>
+            <Caption style={styles.caption}>Streak Days</Caption>
           </View>
-        </View>
 
-        <View style={styles.menuWrapper}>
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="heart-outline" color="#FF6347" size={25} />
-              <Text style={styles.menuItemText}>Your Favorites</Text>
-            </View>
-          </TouchableRipple>
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="credit-card" color="#FF6347" size={25} />
-              <Text style={styles.menuItemText}>Payment</Text>
-            </View>
-          </TouchableRipple>
-          {/* <TouchableRipple onPress={myCustomShare}>
-            <View style={styles.menuItem}>
-              <Icon name="share-outline" color="#FF6347" size={25} />
-              <Text style={styles.menuItemText}>Tell Your Friends</Text>
-            </View>
-          </TouchableRipple> */}
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="account-check-outline" color="#FF6347" size={25} />
-              <Text style={styles.menuItemText}>Support</Text>
-            </View>
-          </TouchableRipple>
-          <TouchableRipple onPress={() => {}}>
-            <View style={styles.menuItem}>
-              <Icon name="settings-outline" color="#FF6347" size={25} />
-              <Text style={styles.menuItemText}>Settings</Text>
-            </View>
-          </TouchableRipple>
-        </View>
+          <View style={styles.infoBox}>
+            <Title style={styles._header_title}>240</Title>
+            <Caption style={styles.caption}>Total Minutes</Caption>
+          </View>
+          
+          <View style={styles.infoBox}>
+            <Title style={styles._header_title}>20</Title>
+            <Caption style={styles.caption}>Sessions</Caption>
+          </View>
 
-        {/* <View>
+      </View>
+
+      <View style={styles.menuWrapper}>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="credit-card" color="#777" size={25}/>
+            <Text style={styles.menuItemText}>Payment</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={styles}>
+          <View style={styles.menuItem}>
+            <Icon name="share-outline" color="#777" size={25}/>
+            <Text style={styles.menuItemText}>Share Your Achievements</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="history" color="#777" size={25}/>
+            <Text style={styles.menuItemText}>Sessions History</Text>
+          </View>
+        </TouchableRipple>
+        <TouchableRipple onPress={() => {}}>
+          <View style={styles.menuItem}>
+            <Icon name="cog" color="#777" size={25}/>
+            <Text style={styles.menuItemText}>Settings</Text>
+          </View>
+        </TouchableRipple>
+      </View>
+
+    </View>
+  
+
+        /*{<View>
           <View style={styles._list}>
             <Text style={styles._th}>Streak Days</Text>
             <Text style={styles._td}>3 Days</Text>
@@ -129,49 +81,61 @@ export default class Profile extends React.Component {
 
         <TouchableOpacity style={[styles._btn]}>
           <Text style={styles._btn_text}>Ready to Premium?</Text>
-        </TouchableOpacity> */}
-      </View>
+        </TouchableOpacity> 
+      </View>*/
     );
   }
 }
 
 let styles = StyleSheet.create({
-  _container: {
-    flex: 1,
-    backgroundColor: theme.black,
-  },
   container: {
     flex: 1,
+    backgroundColor: theme.black,
   },
   userInfoSection: {
     paddingHorizontal: 30,
     marginBottom: 25,
   },
   title: {
-    fontSize: 24,
-    fontWeight: "bold",
+    color: theme.white,
+    fontFamily: theme.extrabold,
+    flex: 1,
+    textAlign: "center",
+    marginRight: 40,
+  },
+  _header_title: {
+    color: theme.white,
+    fontFamily: theme.extrabold,
+    flex: 1,
+    textAlign: "center",
+    fontSize: 28,
+  },
+  _header: {
+    flexDirection: "row",
+    alignItems: "center",
   },
   caption: {
     fontSize: 14,
-    lineHeight: 14,
-    fontWeight: "500",
+    lineHeight: 26,
+    fontWeight: "600",
+    color: theme.white,
   },
   row: {
     flexDirection: "row",
     marginBottom: 10,
   },
   infoBoxWrapper: {
-    borderBottomColor: "#dddddd",
-    borderBottomWidth: 1,
-    borderTopColor: "#dddddd",
-    borderTopWidth: 1,
     flexDirection: "row",
-    height: 100,
+    height: 70,
+    marginBottom: 20,
   },
   infoBox: {
-    width: "50%",
+    width: "33.33%",
     alignItems: "center",
     justifyContent: "center",
+    color: theme.white,
+    fontFamily: theme.extrabold,
+    flex: 1,
   },
   menuWrapper: {
     marginTop: 10,
@@ -182,7 +146,7 @@ let styles = StyleSheet.create({
     paddingHorizontal: 30,
   },
   menuItemText: {
-    color: "#777777",
+    color: theme.white,
     marginLeft: 20,
     fontWeight: "600",
     fontSize: 16,
