aac2b1820ece67888c86cac0ad5f0bf73076fe4d
Amal001001 <amalalmutairi001@gmail.com>
2022-03-13 05:32:46 +0300

    sessions history

---
+++ model/Sessions_History.js
@@ -0,0 +1,27 @@
+export default Sessions_History = [
+    {
+        id: 1,
+        title: 'session title',
+        details: 'this session about...'
+    },
+    {
+        id: 2,
+        title: 'session title',
+        details: 'this session about...'
+    },
+    {
+        id: 3,
+        title: 'session title',
+        details: 'this session about...'
+    },
+    {
+        id: 4,
+        title: 'session title',
+        details: 'this session about...'
+    },
+    {
+        id: 5,
+        title: 'session title',
+        details: 'this session about...'
+    },
+];
